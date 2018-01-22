06f93aa5da9d6bf7d9d0811b514150903c7d9a5b
Casey Chan <caseygocoding@gmail.com>
2018-01-22 11:21:48 -0500

    third

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const router = require('./routes');
 const morgan = require('morgan');
 const logger = morgan('dev');
 const nunjucks = require('nunjucks');
+const models = require('./models');
 
 app.engine('html', nunjucks.render);
 app.set('view engine', 'html');
@@ -19,6 +20,23 @@ app.use(express.static(__dirname + '/public'));
 // Route
 app.use('/', router);
 
-app.listen(3000, function() {
+
+// models.User.sync({})
+// .then(function() {
+// 	return models.Page.sync({})
+// })
+// .then(function() {
+// 	app.listen(3000, function() {
+// 	console.log('Listening on port 3000');
+// 	});
+// })
+// .catch(console.error);
+
+models.db.sync({})
+.then(function () {
+    // make sure to replace the name below with your express app
+	app.listen(3000, function() {
 	console.log('Listening on port 3000');
-});
+	});
+})
+.catch(console.error);
